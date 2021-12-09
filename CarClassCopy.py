e24dde016018afb47d5c269105e971410452cb83
imailpradeep <imailpradeep1@gmail.com>
2021-12-09 14:48:20 +0530

    Add files via upload

---
+++ CarClassCopy.py
@@ -0,0 +1,31 @@
+import pygame
+
+RED=(250,0,0)
+
+class SpeedyBlue(pygame.sprite.Sprite):
+     
+    def __init__(self):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+ 
+        # Instead we could load a proper picture of a car...
+        self.image = pygame.image.load("car1.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (45, 70))
+ 
+        # Fetch the rectangle object that has the dimensions of the image.
+        self.rect = self.image.get_rect()
+        
+        pygame.draw.rect(self.image, RED, [0, 0, self.rect.width, self.rect.height], 3)
+        
+
+    def moveRight(self, pixels):
+        self.rect.x += pixels
+ 
+    def moveLeft(self, pixels):
+        self.rect.x -= pixels
+ 
+    def moveForward(self, pixels): 
+        self.rect.y -= pixels
+ 
+    def moveBackward(self, pixels): 
+        self.rect.y += pixels
